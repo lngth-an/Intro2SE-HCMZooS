a0bf6360b4715439b60b66c38e14058d0758258f
V1nk4n <vinhan188@gmail.com>
2025-06-13 21:24:17 +0700

    fix: pool

--- src/server/src/config/database.js
+++ src/server/src/config/database.js
@@ -18,6 +18,12 @@ const sequelize = new Sequelize(
         rejectUnauthorized: true,
       },
     },
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    },
     logging: process.env.NODE_ENV === 'development' ? console.log : false,
   }
 );
